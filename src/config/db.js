330b46adf8815bb0f064120740186a798113e67e
Karthika <karthikakg2903@gmail.com>
2025-01-29 03:24:26 -0800

    second commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,31 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB connected');
+//   } catch (error) {
+//     console.error('MongoDB connection failed:', error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB connected');
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error('MongoDB connection failed:', error);
+    console.error(`MongoDB connection failed: ${error.message}`);
     process.exit(1);
   }
 };
